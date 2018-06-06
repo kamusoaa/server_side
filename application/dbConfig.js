99284db2aef49e066139d943e6eef611641ca800
kozjava <andrei.drazdou@gmail.com>
2018-06-06 19:41:46 +0300

    fix db path

--- application/dbConfig.js
+++ application/dbConfig.js
@@ -1,6 +1,6 @@
 
 
 module.exports = {
-  //  'uri' : 'mongodb://kozjava:DEN1212184@ds117590.mlab.com:17590/smart_signaling'
-    'uri' : 'mongodb://localhost:27017/smart_signaling'
+  'uri' : 'mongodb://kozjava:DEN1212184@ds117590.mlab.com:17590/smart_signaling'
+    //'uri' : 'mongodb://localhost:27017/smart_signaling'
 };
